db0210757c3948e2ad51f85e09ac3e00e101ec4f
Raúl Martín <raul.martin@urjc.es>
2022-03-01 16:56:32 +0100

    Reproduble statistical tests

--- statistical-tests/statistical_test.R
+++ statistical-tests/statistical_test.R
@@ -2,24 +2,42 @@
 if (!require("devtools")) {
   install.packages("devtools")
 }
+
+if(!require("scmamp")){
+  install.packages("scmamp")
+}
+
+if(!require("ggplot2")){
+  install.packages("ggplot2")
+}
+
+if(!require("readxl")){
+  install.packages("readxl")
+}
+
+library("scmamp")
+library("devtools")
+library("ggplot2")
+library("readxl")
+
+devtools::install_github("b0rxa/scmamp")
+
+set.seed(0)
 options(mc.cores = 8)
-rstan_options(auto_write = TRUE)
-devtools::install_github("b0rxa/scmamp", force=TRUE)
+#rstan_options(auto_write = TRUE)
 
 getCredibleIntervalsWeights <- function(posterior.samples, interval.size=0.9) {
   qmin <- (1-interval.size)/2
   qmax <- 1-qmin
-  lower.bound <- apply(posterior.samples, MARGIN=2, FUN=quantile, p=qmin) 
-  upper.bound <- apply(posterior.samples, MARGIN=2, FUN=quantile, p=qmax) 
-  expectation <- apply(posterior.samples, MARGIN=2, FUN=mean) 
+  lower.bound <- apply(posterior.samples, MARGIN=2, FUN=quantile, p=qmin)
+  upper.bound <- apply(posterior.samples, MARGIN=2, FUN=quantile, p=qmax)
+  expectation <- apply(posterior.samples, MARGIN=2, FUN=mean)
   return (data.frame(Expected=expectation, Lower_bound=lower.bound, Upper_bound=upper.bound))
 }
-library("scmamp")
+
 
 ## Load data
-setwd("path to the folder containing the csv or xlsx with the results to be compared")
 #df <- read.csv("drflp.csv")
-library("readxl")
 df <- read_excel("drflp.xlsx")
 
 ## Statistical Analysis
@@ -30,8 +48,6 @@ pl_model$expected.mode.rank
 pl_model$expected.win.prob
 
 ## Plot
-library("ggplot2")
 processed.results <- getCredibleIntervalsWeights(pl_model$posterior.weights, interval.size=0.9)
 df <- data.frame(Algorithm=rownames(processed.results), processed.results)
 ggplot(df, aes(y=Expected, ymin=Lower_bound, ymax=Upper_bound, x=Algorithm)) + geom_errorbar() + geom_point(col="darkgreen", size=2) +  theme_bw() + coord_flip() + labs(y="Probability of winning")
-
